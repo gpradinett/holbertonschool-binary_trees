8df3fd1263bc29e56c41f94e9a6e0fec68212deb
gpradinett <gpradinett@gmail.com>
2022-07-22 22:49:24 -0500

    1️⃣4️⃣add documemts

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,50 +8,59 @@
 * Return: return the height or 0 if tree is NULL.
 */
 
-
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int number = 0;
-	int left = 0;
-	int right = 0;
+	size_t height_left = 0, height_right = 0;
 
 	/* Caso base para salir de la recursividad */
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left = binary_tree_height(tree->left) + 1;
+	/**
+	 * Si el siguiente nodo tanto del lado izq como del der es NULL significa
+	 * que es una leaf (es el ultimo nodo) MODIFICAMOS esta parte para que
+	 * retorne 1 en lugar de 0. En este caso lo modificamos para que retorne 1
+	 * porque queremos que nos cuente ese nodo para poder ver la diferencia de
+	 * alturas entre el lado izq y el der para ver el balance factor.
+	 */
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 
-	if (tree->right != NULL)
-		right = binary_tree_height(tree->right) + 1;
+	/* Contamos el height de forma recursiva, sumamos 1 cada pasada: */
+	height_left = 1 + binary_tree_height(tree->left);
+	height_right = 1 + binary_tree_height(tree->right);
 
-	number = left - right;
-
-	return (number);
+	/* Si el lado izq es mayor al der entonces retornar lado izq */
+	if (height_left >= height_right)
+	{
+		return (height_left);
+	}
+	else
+	{
+		return (height_right);
+	}
 }
 
 /**
- * binary_tree_height - function that measures the height of a binary tree
- *
- * @tree: is a pointer to the root node of the tree to measure the height
- * Return: number
- */
+* binary_tree_balance - function that measures the balance factor of a binary
+* tree
+* @tree: pointer to the root node of the tree to measure the balance factor
+* Return: balance factor or 0 if tree is NULL
+*/
 
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left = binary_tree_height(tree->left) + 1;
+	/* Modificamos la funcion que hicimos en la task 9 para contar la altura */
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (tree->right != NULL)
-		right = binary_tree_height(tree->right) + 1;
-
-	if (left > right)
-		return (left);
-
-	return (right);
+	/* Se restan las 2 alturas para obtener el balance */
+	return (left_height - right_height);
 }
